ac90a48f19d5df4758992914ba5ad68747fe8db3
SMD.SeanDavis@gmail.com <SMD.SeanDavis@gmail.com@652ab025-80c7-60e5-88d6-9a246ffd9a3e>
2012-01-18 13:53:07 +0000

    Fixed small bug in setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from distutils.core import setup
 import py2exe
 import os
 
-os.remove('fixfixer_gui.exe')
-
 setup(console=['fixfixer_main.py'])
 
 os.chdir('dist')
-os.remove('fixfixer_gui.exe')
+try:
+	os.remove('fixfixer_gui.exe')
+except:
+	pass
 os.rename( 'fixfixer_main.exe', 'fixfixer_gui.exe')
 os.chdir('..')

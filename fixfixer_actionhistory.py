55350fb63ce87c18bd94051a3ee416a000ccb20a
SMD.SeanDavis@gmail.com <SMD.SeanDavis@gmail.com@652ab025-80c7-60e5-88d6-9a246ffd9a3e>
2012-01-27 16:31:28 +0000

    Added enabling and disabling of Undo/Redo when necessary. Added docstring comments to the fixfixer_actionhistory.py

--- fixfixer_actionhistory.py
+++ fixfixer_actionhistory.py
@@ -1,30 +1,43 @@
 class FixAction():
+	"""A class that represents an action taken by the user.
+	
+	The data is indexed as follows:
+	
+		Action              Index    Data
+		clear               data[0]  MarketData value
+							data[1]  MessageTree value
+		market_data_edit    data[0]  Previous MarketData value
+							data[1]  Current MarketData value
+		message_tree_edit   data[0]  MessageTree Index
+							data[1]  Previous MessageTree Index Label
+							data[2]  Current MessageTree Index Label
+		message_tree_paste  data[0]  MessageTree Index
+							data[1]  Previous MessageTree Index Label
+							data[2]  Current MessageTree Index Label
+		message_tree_insert data[0]  Previous MessageTree Contents
+							data[1]  Current MessageTree Contents
+		message_tree_delete data[0]  Previous MessageTree Contents
+							data[1]  Current MessageTree Contents
+		message_tree_move   data[0]  Previous MessageTree Contents
+							data[1]  Current MessageTree Contents"""
 	def __init__(self, parent, name, data):
 		self.parent = parent
 		self.name = name
 		self.data = data
 		
 	def UndoAction(self):
+		"""The action to perform if Undo() is called."""
 		if self.name == 'clear':
-			# if 'clear', then we want to fill everything back in.
-			# data[0] = previous market_data value
-			# data[1] = previous message_tree data
 			self.parent.market_data.SetValue(self.data[0])
 			self.parent.message_tree.set_message(self.data[1])
 			
 		elif self.name == 'market_data_edit':
-			# data[0] = previous market_data value
-			# data[1] = current market_data value
 			self.parent.market_data.ChangeValue(self.data[0])
-			
 			
 		elif self.name == 'message_tree_edit':
 			self.parent.message_tree.SetItemText(self.data[0], self.data[1])
 			
 		elif self.name == 'message_tree_paste':
-			# data[0] = wxTreeId
-			# data[1] = old value
-			# data[2] = new value
 			self.parent.message_tree.SetItemText(self.data[0], self.data[1])
 		
 		elif self.name == 'message_tree_insert':
@@ -37,6 +50,7 @@ class FixAction():
 			self.parent.message_tree.set_message(self.data[0])
 	
 	def RedoAction(self):
+		"""The action to perform if Redo() is called."""
 		if self.name == 'clear':
 			self.parent.market_data.SetValue("")
 			self.parent.message_tree.clear()
@@ -60,6 +74,8 @@ class FixAction():
 			self.parent.message_tree.set_message(self.data[1])
 
 class FixActionHistory():
+	"""The Fix Action Server class.  Contains information on all of the
+	applications history.  Handles requests to add, clear, and query the history."""
 	def __init__(self, parent):
 		self.parent = parent
 		self.UndoHistory = []
@@ -68,47 +84,62 @@ class FixActionHistory():
 		self.StartAdd_data = None
 		
 	def Undo(self):
+		"""When Undo() is called, perform the action and append it to Redo, enabling
+		and disabling each as needed."""
 		try:
 			action = self.UndoHistory.pop()
 			action.UndoAction()
 			self.RedoHistory.append(action)
+			self.parent.Redo.Enable(True)
 		except:
 			pass
+		if len(self.UndoHistory) == 0: self.parent.Undo.Enable(False)
 		
 	def Redo(self):
+		"""When Redo() is called, perform the action and append it to Undo, enabling
+		and disabling each as needed."""
 		try:
 			action = self.RedoHistory.pop()
 			action.RedoAction()
 			self.UndoHistory.append(action)
+			self.parent.Undo.Enable(True)
 		except:
 			pass
+		if len(self.RedoHistory) == 0: self.parent.Redo.Enable(False)
 		
 	def GetUndoHistory(self):
+		"""Return the Undo history."""
 		return self.UndoHistory
 		
 	def GetRedoHistory(self):
+		"""Return the Redo history."""
 		return self.RedoHistory
 		
 	def ClearHistory(self):
+		"""Clear Undo and Redo histories, disabling each."""
 		self.UndoHistory = []
 		self.RedoHistory = []
 		self.StartAdd_name = None
 		self.StartAdd_data = None
+		self.parent.Undo.Enable(False)
+		self.parent.Redo.Enable(False)
+		
 		
 	def Write(self, name, data):
+		"""Add an item to the Undo history, clearing the Redo history."""
 		action = FixAction(self.parent, name, data)
 		self.UndoHistory.append(action)
 		self.RedoHistory = []
-#		print "Undo:\n" + str(self.UndoHistory)
-#		print "Redo:\n" + str(self.RedoHistory)
+		self.parent.Undo.Enable(True)
+		self.parent.Redo.Enable(False)
 		
 	def StartAdd(self, name, data):
-#		print "StartAdd: %s, %s" % (name, data)
+		"""StartAdd() is used to begin adding data when only part of it is available."""
 		self.StartAdd_name = name
 		self.StartAdd_data = data
 		
 	def EndAdd(self, name, data):
-#		print "EndAdd: %s, %s" % (name, data)
+		"""Ends the StartAdd() with additional data."""
 		if name == self.StartAdd_name:
 			if type(data) == type(list()):
 				self.Write(name, self.StartAdd_data + data )

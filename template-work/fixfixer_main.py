070e9eda4785b8f03e44a9ce2273941f6b95402b
cpuobsessed <cpuobsessed@652ab025-80c7-60e5-88d6-9a246ffd9a3e>
2012-01-31 16:35:59 +0000

    Creating templates for the wizard

---
+++ template-work/fixfixer_main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import wx
+import fixfixer_gui
+import fixfixer_xml
+
+
+
+if __name__ == "__main__":
+	app = wx.PySimpleApp(0)
+	wx.InitAllImageHandlers()
+	frame_1 = fixfixer_gui.FixFixerGui(None, -1, "")
+	app.SetTopWindow(frame_1)
+	frame_1.Show()
+	app.MainLoop()
